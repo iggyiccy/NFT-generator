9bb3888e4cbc1bf4b128075dcb4df044430b6b40
iggyi <iccyn@pm.me>
2021-09-27 00:44:25 +1000

    added monster card

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useEffect, useState, useRef } from "react";
 import { ethers } from "ethers";
 import { hasEthereum } from "../utils/ethereum";
@@ -97,20 +98,12 @@ export default function Home() {
     setNewGreetingState("");
   }
 
-  // function autoCheckRadioButton() {
-  //   var radios = document.querySelectorAll("input[type=radio]");
-  //   for (var i = 0; i < radios.length; i++) {
-  //     var randomNumber = Math.random() * 10 > 5;
-  //     radios[i].checked = randomNumber ? true : false;
-  //   }
-  // }
-
   function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   return (
-    <div className="max-w-lg mt-36 mx-auto text-center px-4">
+    <div className="max-w-lg mt-36 mx-auto">
       <Head>
         <title>Personalised NFT</title>
         <meta name="description" content="Get Your Personalised NFT" />
@@ -125,15 +118,14 @@ export default function Home() {
           </p>
         ) : (
           <>
-            <h1 className="text-4xl font-semibold mb-8">
-              🔮 Get Your Personalised NFT
+            <h1 className="text-4xl text-center font-black mb-8">
+              🔮 Your Personalised NFT
             </h1>
-            <div className="h-4">
+            <div className="h-4 text-center font-thin">
               {connectedWalletAddress && (
                 <p className="text-md">{connectedWalletAddress}</p>
               )}
             </div>
-            {/* <button onClick={autoCheckRadioButton}>Auto Fill</button> */}
             <Form
               onSubmit={onSubmit}
               render={({
@@ -143,43 +135,122 @@ export default function Home() {
                 pristine,
                 submitting,
               }) => (
-                <form onSubmit={handleSubmit}>
+                <form
+                  onSubmit={handleSubmit}
+                  className=" bg-white shadow rounded-lg p-10"
+                >
+                  <div className="grid gap-3 grid-cols-5 mb-10">
+                    <img
+                      src="/angry-1991061.svg"
+                      className="h-8 w-8"
+                      alt="1.angry"
+                    />
+                    <img
+                      src="/sad-1991063.svg"
+                      className="h-8 w-8"
+                      alt="2.sad"
+                    />
+                    <img
+                      src="/care-1991058.svg"
+                      className="h-8 w-8"
+                      alt="3.care"
+                    />
+                    <img
+                      src="/like-1991059.svg"
+                      className="h-8 w-8"
+                      alt="4.like"
+                    />
+                    <img
+                      src="/love-1991064.svg"
+                      className="h-8 w-8"
+                      alt="5.love"
+                    />
+                  </div>
                   {questions.map((question) => (
-                    <div key={question.index} className="inline-flex mt-4">
-                      {question.question}
-                      {score.map((scores) => (
+                    <div key={question.index} className="block mt-4">
+                      <label className="mt-3 inline-block text-left font-semibold text-lg">
+                        {question.question}
+                      </label>
+                      <div className="mt-3 grid gap-6 grid-cols-5">
                         <label
                           id="radioButton"
-                          key={scores.Score}
-                          className="inline-flex items-center ml-4"
+                          className="inline-flex items-center"
                         >
                           <Field
+                            id={question.question}
                             name={question.question}
                             component="input"
                             type="radio"
-                            className="form-radio h-5 w-5 text-purple-600"
-                            value={scores.Score}
-                            defaultValue={randomIntFromInterval(1, 5)}
-                            // checked
+                            className="form-radio h-5 w-5 text-purple-600 hover:text-purple-400"
+                            value="1"
                           />
-                          {/* <span className="ml-2 text-gray-700">
-                            {scores.Description}
-                          </span> */}
                         </label>
-                      ))}
+                        <label
+                          id="radioButton"
+                          className="inline-flex items-center"
+                        >
+                          <Field
+                            id={question.question}
+                            name={question.question}
+                            component="input"
+                            type="radio"
+                            className="form-radio h-5 w-5 text-purple-600 hover:text-purple-400"
+                            value="2"
+                          />
+                        </label>
+                        <label
+                          id="radioButton"
+                          className="inline-flex items-center"
+                        >
+                          <Field
+                            id={question.question}
+                            name={question.question}
+                            component="input"
+                            type="radio"
+                            className="form-radio h-5 w-5 text-purple-600 hover:text-purple-400"
+                            value="3"
+                          />
+                        </label>
+                        <label
+                          id="radioButton"
+                          className="inline-flex items-center"
+                        >
+                          <Field
+                            id={question.question}
+                            name={question.question}
+                            component="input"
+                            type="radio"
+                            className="form-radio h-5 w-5 text-purple-600 hover:text-purple-400"
+                            value="4"
+                          />
+                        </label>
+                        <label
+                          id="radioButton"
+                          className="inline-flex items-center"
+                        >
+                          <Field
+                            id={question.question}
+                            name={question.question}
+                            component="input"
+                            type="radio"
+                            className="form-radio h-5 w-5 text-purple-600 hover:text-purple-400"
+                            value="5"
+                          />
+                        </label>
+                      </div>
                     </div>
                   ))}
-                  {/* <pre className="mt-4 mb-4">
-                    {JSON.stringify(values, 0, 2)}
-                  </pre> */}
-                  <div className="mt-8">
-                    <button
-                      className="py-3 w-64 text-xl text-white bg-purple-400 rounded-2xl"
-                      type="submit"
-                      disabled={pristine || submitting}
-                    >
-                      Submit
-                    </button>
+                  <div className="mt-10 text-center">
+                    <Link href="/result">
+                      <button
+                        component="a"
+                        className="py-3 w-64 text-xl text-white bg-purple-400 hover:bg-purple-600 font-black rounded-2xl"
+                        type="submit"
+                        disabled={pristine || submitting}
+                      >
+                        Submit
+                      </button>
+                    </Link>
                   </div>
                 </form>
               )}
@@ -188,12 +259,12 @@ export default function Home() {
         )}
       </main>
 
-      <footer className="mt-20">
+      <footer className="mt-20 text-center">
         <a
           href="http://rchow.dev"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-600 hover:text-blue-700"
+          className="text-gray-500 hover:text-gray-800"
         >
           Source code on GitHub
         </a>
---
+++ pages/result.js
@@ -0,0 +1,75 @@
+import Head from "next/head";
+import connectedWalletAddress from "./index";
+// {
+//   "Paint or draw": "5"
+// }
+
+export default function Result() {
+  return (
+    <div className="max-w-lg mt-36 mx-auto">
+      <Head>
+        <title>Personalised NFT</title>
+        <meta name="result" content="personality result" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="space-y-48">
+        {!process.env.NEXT_PUBLIC_GREETER_ADDRESS ? (
+          <p className="text-md">
+            Please add a value to the <pre>NEXT_PUBLIC_GREETER_ADDRESS</pre>{" "}
+            environment variable.
+          </p>
+        ) : (
+          <>
+            <h1 className="text-4xl text-center font-black mb-80">
+              🔮 Your Personalised NFT
+            </h1>
+            {/* <div className="container bg-white">.</div> */}
+            {/* !!Doesn't work!! */}
+            {/* <div className="h-4 text-center font-thin">
+              {connectedWalletAddress && (
+                <p className="text-md">{connectedWalletAddress}</p>
+              )}
+            </div> */}
+            <div className="p-10 bg-gradient-to-br from-purple-50 to-white shadow-xl rounded-lg my-20">
+              <div className="flex justify-center -mt-80">
+                <img
+                  className="w-50 h-70 object-cover rounded-lg border-2 border-purple-600"
+                  src="https://images.pokemontcg.io/xy1/1.png"
+                />
+              </div>
+              <div>
+                <h2 className="mt-10 text-gray-800 text-3xl font-semibold">
+                  Monster Name
+                </h2>
+                <p className="mt-2 text-gray-600">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                  dolores deserunt ea doloremque natus error, rerum quas odio
+                  quaerat nam ex commodi hic, suscipit in a veritatis pariatur
+                  minus consequuntur!
+                </p>
+              </div>
+              <div className="flex justify-end mt-4">
+                <a
+                  href="#"
+                  className="text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-purple-600"
+                >
+                  Mint Now
+                </a>
+              </div>
+            </div>
+          </>
+        )}
+      </main>
+      <footer className="mt-20 text-center">
+        <a
+          href="http://rchow.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-gray-800"
+        >
+          Source code on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
